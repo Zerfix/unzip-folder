f80084830b7ec3e0f040bafa1c47b299b1436dba
zerfix <zerfix@protonmail.com>
2017-10-11 15:18:03 +0200

    now works with enumerated .rar

--- unzip.bash
+++ unzip.bash
@@ -1,3 +1,3 @@
-if 7z x -y $(find . -name *.7z -o -name *.rar -o -name *.zip);
-then rm $(find . -name *.7z -o -name *.rar -o -name *.zip)
+if 7z x -y $(find . -name *.7z -o -name *.rar* -o -name *.zip);
+then rm $(find . -name *.7z -o -name *.rar* -o -name *.zip)
 fi
