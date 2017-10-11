873ee6bddc9107d4ef468e40a5ae47adb727df92
zerfix <zerfix@protonmail.com>
2017-10-11 15:15:05 +0200

    now works with subfolders

--- unzip.bash
+++ unzip.bash
@@ -1,3 +1,3 @@
-if 7z x -y $(grep -r . -e *.7z -e *.rar -e *.zip);
-then rm $(grep -r . -e *.7z -e *.rar -e *.zip)
+if 7z x -y $(find . -name *.7z -o -name *.rar -o -name *.zip);
+then rm $(find . -name *.7z -o -name *.rar -o -name *.zip)
 fi
